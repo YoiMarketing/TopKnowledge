bcf97b5321f16352c865f86d1e6cff4c18084916
YoiMarketing <97491417+YoiMarketing@users.noreply.github.com>
2022-04-12 14:29:02 +0800

    update js
    redirect host top58.net

---
+++ angle-change.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    $(".js-btn-angle").click(function () {
+        var data_angle = $(this).attr("data-angle");
+        if (data_angle === "down") {
+            $(this).attr("data-angle", "up");
+            $(this).find("i").removeClass("fa-angle-down");
+            $(this).find("i").addClass("fa-angle-up");
+        }
+        else {
+            $(this).attr("data-angle", "down");
+            $(this).find("i").removeClass("fa-angle-up");
+            $(this).find("i").addClass("fa-angle-down");
+        }
+    });
+    var host = "top58.net";
+    if (window.location.hostname != host && window.location.hostname != "localhost") {
+        location.href = "https://" + host;
+    }
+})
